8cf90d69af222c5fd569ee434c86f8a1cad2e332
Anna Myshkina <annet.obuchova@gmail.com>
2025-09-08 20:09:02 +0300

    add module manual_games.py

---
+++ brain_games/games/manual_games.py
@@ -0,0 +1,10 @@
+
+def game_instruction(number_game):
+    if number_game == 1:
+        return 'Answer "yes" if the number is even, otherwise answer "no".'
+    elif number_game == 2:
+        return "What is the result of the expression?"
+    elif number_game == 3:
+        return 'Find the greatest common divisor of given numbers.'
+    else:
+        return "There is no such number. Let's try again!"
