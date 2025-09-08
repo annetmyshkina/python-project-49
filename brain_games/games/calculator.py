44a92c79ff870b0e3ab2854b27f1f9a054a94107
Anna Myshkina <annet.obuchova@gmail.com>
2025-09-08 19:02:13 +0300

    add new game module calculator.py

---
+++ brain_games/games/calculator.py
@@ -0,0 +1,26 @@
+
+from random import randint, choice
+
+
+def calculator():
+    
+    x = randint(0, 100)
+    y = randint(0, 100)
+    operator = choice(['+', '-', '*'])
+
+    match operator:
+        case '+':
+            random_expression = f'{x} + {y}'
+            correct_answer = str(x + y)
+            return (random_expression, correct_answer)
+        case '-':
+            random_expression = f'{x} - {y}'
+            correct_answer = str(x - y)
+            return (random_expression, correct_answer)
+        case '*':
+            random_expression = f'{x} * {y}'
+            correct_answer = str(x * y)
+            return (random_expression, correct_answer)
+        
+
+    
