28d00901c820f9e81e0d091d7f4276fea6a4746f
Anna Myshkina <annet.obuchova@gmail.com>
2025-09-09 19:41:23 +0300

    add new module brain_calc.py

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,25 @@
+from random import choice, randint
+
+
+def get_description():
+    return 'What is the result of the expression?'
+
+
+def generate_question():
+    x = randint(0, 100)
+    y = randint(0, 100)
+    operator = choice(["+", "-", "*"])
+
+    match operator:
+        case "+":
+            random_expression = f"{x} + {y}"
+            correct_answer = str(x + y)
+            return (random_expression, correct_answer)
+        case "-":
+            random_expression = f"{x} - {y}"
+            correct_answer = str(x - y)
+            return (random_expression, correct_answer)
+        case "*":
+            random_expression = f"{x} * {y}"
+            correct_answer = str(x * y)
+            return (random_expression, correct_answer)
