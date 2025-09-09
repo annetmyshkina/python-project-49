9177e0d9ffb14efde259921b889e3124c653d533
Anna Myshkina <annet.obuchova@gmail.com>
2025-09-09 10:58:06 +0300

    add module brain_progression

---
+++ brain_games/games/brain_progression.py
@@ -0,0 +1,18 @@
+
+
+from random import randint, choice
+
+def encrypt_sequence():
+    sequence = get_progression()
+    random_element = str(choice(sequence)) # выбираем случайный элемент из прогрессии методом рандома, меняем тип на str 
+    question_sequence = ' '.join(['..' if j == random_element else j for j in sequence]) # шифруем случайный элемент в списке и распаковываем для вывода игроку
+    return question_sequence, random_element
+
+
+def get_progression():
+    length_progression = choice([5, 6, 7, 8, 9, 10, 11, 12])
+    start = randint(0, 20)
+    step = randint(1, 10) # шаг не должен быть нулевым
+    # генерируем список из lenght_progression с шагом step
+    a_progression = [str(start + i * step) for i in range(length_progression)]
+    return a_progression
