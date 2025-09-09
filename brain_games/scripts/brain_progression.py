21d2eb7088e1ed484cda747e79de991b50bd7fd9
Anna Myshkina <annet.obuchova@gmail.com>
2025-09-09 19:37:14 +0300

    add new script brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,10 @@
+from brain_games.games import brain_progression
+from brain_games.games.start_game import start_game
+
+
+def main():
+    start_game(brain_progression)
+
+
+if __name__ == "__main__":
+    main()
