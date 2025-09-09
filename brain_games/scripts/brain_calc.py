0aaa126a02599b728690bde0d43c013f6fb51007
Anna Myshkina <annet.obuchova@gmail.com>
2025-09-09 19:39:30 +0300

    add new script brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+from brain_games.games import brain_calc
+from brain_games.games.start_game import start_game
+
+
+def main():
+    start_game(brain_calc)
+
+
+if __name__ == "__main__":
+    main()
